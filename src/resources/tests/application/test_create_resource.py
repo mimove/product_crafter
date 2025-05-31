89d43b6446c271a60b15fcb6babbfe51e139478c
Miguel Moratilla Vega <mimove14@gmail.com>
2025-05-31 18:39:41 +0200

    Red test for invalid urls in creating a resource

--- src/resources/tests/application/test_create_resource.py
+++ src/resources/tests/application/test_create_resource.py
@@ -1,4 +1,6 @@
+import pytest
 from resources.application.create_resource import CreateResource, CreateResourceCommand
+from resources.domain.exceptions import UrlIsNotValid
 from resources.domain.models import Resource
 from resources.domain.repositories import ResourcesRepository
 
@@ -18,10 +20,21 @@ class TestCreateResource:
         resource_repository = FakeResourceRepository()
 
         CreateResource(resource_repository).execute(
-            CreateResourceCommand(resource_url="resource-url-example")
+            CreateResourceCommand(resource_url="http://google.com")
         )
         resources = resource_repository.all()
         assert len(resources) == 1
-        assert resources[0].url() == "resource-url-example"
+        assert resources[0].url() == "http://google.com"
+
+    def test_raise_exception(self) -> None:
+        resource_repository = FakeResourceRepository()
+
+        with pytest.raises(UrlIsNotValid):
+            CreateResource(resource_repository).execute(
+                CreateResourceCommand(resource_url="not-a-valid-url")
+            )
+        resources = resource_repository.all()
+        assert len(resources) == 0
+        assert resources[0].url() == "http://google.com"
 
     
