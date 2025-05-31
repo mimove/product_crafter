7a2cb115c49708a7abee805fe010a95e2cbb6747
Miguel Moratilla Vega <mimove14@gmail.com>
2025-05-31 18:29:12 +0200

    Green test

--- src/resources/application/create_resource.py
+++ src/resources/application/create_resource.py
@@ -1,7 +1,9 @@
+from resources.domain.models import Resource
 from resources.domain.repositories import ResourcesRepository
 
 class CreateResource:
     def __init__(self, resource_repository: ResourcesRepository) -> None:
         self._resource_repository = resource_repository
+
     def execute(self) -> None:
-        pass
+        self._resource_repository.save(Resource())
